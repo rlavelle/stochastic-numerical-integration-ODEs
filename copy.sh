15f335d393b9910c1507afc2f736cb14b6afdff6
rowlavel <rowlavel@iu.edu>
2020-08-19 15:12:19 -0400

    removed copy.sh

--- copy.sh
+++
@@ -1,4 +0,0 @@
-rm -rf protein-trials/*
-rm -rf time-trials/*
-scp -r rowlavel@silo.cs.indiana.edu:/u/rowlavel/stochastic-numerical-integration-ODEs/protein-trials/ .
-scp -r rowlavel@silo.cs.indiana.edu:/u/rowlavel/stochastic-numerical-integration-ODEs/time-trials/ .
