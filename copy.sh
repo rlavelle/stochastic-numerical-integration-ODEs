9f2f87cb806e6858d616a775abc7e19b31b4cae4
rowlavel <rowlavel@iu.edu>
2020-08-19 15:00:11 -0400

    script to copy files over

---
+++ copy.sh
@@ -0,0 +1,4 @@
+rm -rf protein-trials/*
+rm -rf time-trials/*
+scp -r rowlavel@silo.cs.indiana.edu:/u/rowlavel/stochastic-numerical-integration-ODEs/protein-trials/ .
+scp -r rowlavel@silo.cs.indiana.edu:/u/rowlavel/stochastic-numerical-integration-ODEs/time-trials/ .
