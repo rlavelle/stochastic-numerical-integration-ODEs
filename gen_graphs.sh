a0aa2c30e52720de65d276ff3ef0d9c5a826af6a
rowlavel <rowlavel@iu.edu>
2020-11-11 19:13:35 -0500

    scripts to run code in background

---
+++ gen_graphs.sh
@@ -0,0 +1,8 @@
+python3 random_plots.py 50 /home/cvanoeve/stochastic-numerical-integration-ODEs/protein-trials-noclb1-noclb3 scale-15-clb1-clb3-KO &
+python3 random_plots.py 50 /home/cvanoeve/stochastic-numerical-integration-ODEs/protein-trials-noclb3 scale-15-clb3-KO &
+python3 random_plots.py 50 /home/cvanoeve/stochastic-numerical-integration-ODEs/protein-trials-noclb1-noclb3-noclb4 scale-15-full-KO &
+python3 random_plots.py 50 /home/cvanoeve/stochastic-numerical-integration-ODEs/protein-trials-noclb1 scale-15-clb1-KO &
+python3 random_plots.py 50 /home/cvanoeve/stochastic-numerical-integration-ODEs/protein-trials-noclb1-noclb4 scale-15-clb1-clb4-KO &
+python3 random_plots.py 50 /home/cvanoeve/stochastic-numerical-integration-ODEs/protein-trials-noclb4 scale-15-clb4-KO &
+python3 random_plots.py 50 /home/cvanoeve/stochastic-numerical-integration-ODEs/protein-trials-noclb3-noclb4 scale-15-clb3-clb4-KO &
+python3 random_plots.py 50 /home/cvanoeve/stochastic-numerical-integration-ODEs/protein-trials-wt scale-15-WT &
